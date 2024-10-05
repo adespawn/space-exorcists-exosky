f93fe7ea95fff862630b42ce971a524eb3446d84
Stanisław Czech <sc@lo3gdynia.pl>
2024-10-05 11:11:02 +0200

    write file interface

--- backend/databuilder/src/file_operations.h
+++ backend/databuilder/src/file_operations.h
@@ -4,6 +4,9 @@
 #include <bits/stdc++.h>
 #include "data_structs.h"
 
+const std::string write_path = "/media/adespawn/01524459-1aff-4407-a4a6-68c6a130a898/data/layer_0";
+
 std::vector<initial_raw_data> read_raw();
+void write_to_file(const internal_data &data, const string &file);
 
 #endif
