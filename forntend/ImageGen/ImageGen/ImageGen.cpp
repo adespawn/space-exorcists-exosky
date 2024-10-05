6c03c12470371a3181b48c803941ce6ac38417c5
NatanTulo <s193527@student.pg.edu.pl>
2024-10-05 11:04:23 +0200

    białe wzory na czarnym tle bmp

---
+++ forntend/ImageGen/ImageGen/ImageGen.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cstdint>
+
+#pragma pack(push, 1) 
+struct BMPFileHeader {
+    uint16_t fileType{ 0x4D42 };
+    uint32_t fileSize{ 0 };
+    uint16_t reserved1{ 0 };
+    uint16_t reserved2{ 0 };
+    uint32_t offsetData{ 54 };
+};
+
+struct BMPInfoHeader {
+    uint32_t size{ 40 };
+    int32_t width{ 0 };
+    int32_t height{ 0 };
+    uint16_t planes{ 1 };
+    uint16_t bitCount{ 24 };
+    uint32_t compression{ 0 };
+    uint32_t sizeImage{ 0 };
+    int32_t xPixelsPerMeter{ 0 };
+    int32_t yPixelsPerMeter{ 0 };
+    uint32_t colorsUsed{ 0 };
+    uint32_t colorsImportant{ 0 };
+};
+
+#pragma pack(pop)
+
+struct RGB {
+    uint8_t blue;
+    uint8_t green;
+    uint8_t red;
+};
+
+bool isWhite(int x, int y);
+
+void generateBitmap(const char* fileName, int width, int height) {
+    BMPFileHeader fileHeader;
+    BMPInfoHeader infoHeader;
+
+    infoHeader.width = width;
+    infoHeader.height = height;
+
+    // Obliczamy rozmiar danych pikseli (z wyrównaniem do 4 bajtów na każdą linię)
+    int rowStride = (width * 3 + 3) & ~3;  // Każda linia musi mieć rozmiar będący wielokrotnością 4 bajtów
+    fileHeader.fileSize = sizeof(BMPFileHeader) + sizeof(BMPInfoHeader) + rowStride * height;
+
+    std::ofstream file(fileName, std::ios::binary);
+    if (file) {
+        // Zapisujemy nagłówki
+        file.write(reinterpret_cast<const char*>(&fileHeader), sizeof(fileHeader));
+        file.write(reinterpret_cast<const char*>(&infoHeader), sizeof(infoHeader));
+
+        RGB blackPixel = { 0, 0, 0 };
+		RGB whitePixel = { 255, 255, 255 };
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                if (isWhite(x,y)) {
+                    file.write(reinterpret_cast<const char*>(&whitePixel), sizeof(whitePixel));
+                }
+                else {
+                    file.write(reinterpret_cast<const char*>(&blackPixel), sizeof(blackPixel));
+                }
+            }
+            // Dodajemy padding, jeśli jest potrzebny
+            uint8_t padding[3] = { 0, 0, 0 };
+            file.write(reinterpret_cast<const char*>(padding), rowStride - width * 3);
+        }
+
+        file.close();
+        std::cout << "Bitmapa została zapisana jako: " << fileName << std::endl;
+    }
+    else {
+        std::cerr << "Nie można utworzyć pliku!" << std::endl;
+    }
+}
+
+bool isWhite(int x, int y) {
+	return (x / 64 + y / 64) % 5 == 0;
+}
+
+int main() {
+    generateBitmap("output.bmp", 2048, 2048); 
+	system("output.bmp");
+    return 0;
+}
