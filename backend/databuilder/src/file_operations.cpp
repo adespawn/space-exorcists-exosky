1c1a1b6a1a2d389c34c4fe971c144b8f64bf399f
Stanisław Czech <sc@lo3gdynia.pl>
2024-10-05 11:27:08 +0200

    write data

--- backend/databuilder/src/file_operations.cpp
+++ backend/databuilder/src/file_operations.cpp
@@ -1,5 +1,8 @@
 #include "file_operations.h"
 #include "error.h"
+#include <fcntl.h>
+
+const std::string write_path = "/media/adespawn/01524459-1aff-4407-a4a6-68c6a130a898/data/layer_0/";
 
 using namespace std;
 
@@ -12,10 +15,57 @@ vector<initial_raw_data> read_raw()
         int cnt = read(0, &element, sizeof(initial_raw_data));
         if (cnt <= 0)
             break;
-        if(cnt != sizeof(initial_raw_data))
+        if (cnt != sizeof(initial_raw_data))
             critical("Didn't read the whole data!");
-        
+
         res.push_back(element);
     }
     return res;
+}
+
+bool registered_atexit = false;
+unordered_map<string, int> bindings;
+
+void close_all_files()
+{
+    for (const auto &[key, value] : bindings)
+        close(value);
+}
+
+void write_n(int fd, const void *vptr, size_t n)
+{
+    ssize_t nleft, nwritten;
+    const char *ptr;
+
+    ptr = (char *)vptr;
+    nleft = n;
+    while (nleft > 0)
+    {
+        if ((nwritten = write(fd, ptr, nleft)) <= 0)
+            critical("There was a serious error while writing a file: " + to_string(nwritten));
+        nleft -= nwritten;
+        ptr += nwritten;
+    }
+    return;
+}
+
+void write_to_file(const internal_data &data, const string &file)
+{
+    if (!registered_atexit)
+    {
+        registered_atexit = true;
+        atexit(close_all_files);
+    }
+
+    if (bindings.count(file) == 0)
+    {
+        string full_path = write_path + file;
+        int fd = open64(full_path.c_str(), O_CREAT | O_APPEND);
+        if(fd <= 0)
+            critical("There was an error while opening a file: "+to_string(fd));
+        
+        bindings[file] = fd;
+    }
+
+    write_n(bindings[file], &data, sizeof(internal_data));
 }
